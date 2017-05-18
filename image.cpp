5f690d66fe2c872102d25fa374a85569464330e6
Diadlo <polsha3@gmail.com>
2017-05-18 09:24:49 +0300

    Remove unused line function

--- image.cpp
+++ image.cpp
@@ -2,39 +2,6 @@
 
 #include <cmath>
 
-void line(QImage& image, int x0, int y0, int x1, int y1, QColor color) {
-    bool steep = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
-    }
-
-    if (x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
-
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    int derror2 = std::abs(dy) * 2;
-    int error2 = 0;
-    int y = y0;
-    for (int x = x0; x <= x1; x++) {
-        if (steep) {
-            image.setPixelColor(y, x, color);
-        } else {
-            image.setPixelColor(x, y, color);
-        }
-
-        error2 += derror2;
-        if (error2 > dx) {
-            y += (y1 > y0 ? 1 : -1);
-            error2 -= dx*2;
-        }
-    }
-}
-
 Image::Image(int w, int h)
     : img(w, h, QImage::Format_RGB32)
 {
